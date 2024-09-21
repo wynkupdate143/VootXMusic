a22ce3ca003e59fb519ce4582bdefa02db965c98
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2024-09-21 13:20:22 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -33,25 +33,25 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "❥—————————"
+        bar = "▷—————————"
     elif 10 < umm < 20:
-        bar = "—❥————————"
+        bar = "—▷————————"
     elif 20 <= umm < 30:
-        bar = "——❥———————"
+        bar = "——▷———————"
     elif 30 <= umm < 40:
-        bar = "———❥——————"
+        bar = "———▷——————"
     elif 40 <= umm < 50:
-        bar = "————❥—————"
+        bar = "————▷—————"
     elif 50 <= umm < 60:
-        bar = "—————❥————"
+        bar = "—————▷————"
     elif 60 <= umm < 70:
-        bar = "——————❥———"
+        bar = "——————▷———"
     elif 70 <= umm < 80:
-        bar = "———————❥——"
+        bar = "———————▷——"
     elif 80 <= umm < 95:
-        bar = "————————❥—"
+        bar = "————————▷—"
     else:
-        bar = "—————————❥"
+        bar = "—————————▷"
     buttons = [
          [
             InlineKeyboardButton(
@@ -66,14 +66,6 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
-            InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/BRANDEDKING8"
-            ),
-            InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/BRANDED_WORLD"
-            ),
-        ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
@@ -87,14 +79,6 @@ def stream_markup(_, videoid, chat_id):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/BRANDEDKING8"
-            ),
-            InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/BRANDED_WORLD"
-            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -243,15 +227,6 @@ def stream_markup2(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-[
-            InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/BRANDEDKING8"
-            ),
-            InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/BRANDED_WORLD"
-            ),
-        ],
-
         [
             InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data="close"),
         ],
