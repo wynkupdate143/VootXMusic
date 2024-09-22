1165559523df7c1f034d22b87068a7dacb9a4f6f
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2024-09-22 10:14:18 +0530

    Update zowner.py

--- BrandrdXMusic/plugins/tools/zowner.py
+++ BrandrdXMusic/plugins/tools/zowner.py
@@ -11,13 +11,13 @@ from BrandrdXMusic.utils.database import add_served_chat, get_assistant
 @app.on_message(filters.command("repo"))
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/1aac9a42f6f35138da34b.jpg",
-        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
+        photo=f"https://te.legra.ph/file/ec19cf227791a167abedc.jpg",
+        caption=f"""🍁 𝐏𝐀𝐇𝐋𝐄 𝐉𝐀𝐊𝐄 𝐀𝐍𝐈𝐌𝐀𝐋 𝐊𝐎 𝐏𝐀𝐏𝐀 𝐁𝐎𝐋 🍁""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://github.com/WCGKING/BrandrdXMusic"
+                        "🍁 sᴏᴜʀᴄᴇ 🍁", url=f"https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
                     )
                 ]
             ]
@@ -28,13 +28,13 @@ async def help(client: Client, message: Message):
 @app.on_message(filters.command("clone"))
 async def clones(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/1aac9a42f6f35138da34b.jpg",
+        photo=f"https://te.legra.ph/file/ec19cf227791a167abedc.jpg",
         caption=f"""**🙂You Are Not Sudo User So You Are Not Allowed To Clone Me.**\n**😌Click Given Below Button And Host Manually Otherwise Contact Owner Or Sudo Users For Clone.**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://github.com/WCGKING/BrandrdXMusic"
+                        "🍁 sᴏᴜʀᴄᴇ 🍁", url=f"https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
                     )
                 ]
             ]
