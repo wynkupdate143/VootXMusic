556db3836bbf687a53335d35c79f5675bf730c13
Animal Janwar <160995009+Githubsoniaryan2@users.noreply.github.com>
2025-01-14 19:45:28 +0530

    Update help.py

--- BrandrdXMusic/plugins/bot/help.py
+++ BrandrdXMusic/plugins/bot/help.py
@@ -10,8 +10,6 @@ from BrandrdXMusic.utils.decorators.language import LanguageStart, languageCB
 from BrandrdXMusic.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
-from BrandrdXMusic.utils.stuffs.helper import Helper
-
 
 @app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
 @app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
