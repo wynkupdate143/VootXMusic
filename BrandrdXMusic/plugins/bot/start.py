21f146f6cbea4733b305cc6123ccfcc1b1b92194
NOBITA <nobitaanetwork@gmail.com>
2025-06-27 22:55:54 +0530

    Update start.py

--- BrandrdXMusic/plugins/bot/start.py
+++ BrandrdXMusic/plugins/bot/start.py
@@ -86,7 +86,7 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await message.reply_sticker("CAACAgUAAxkBAAEQI1hlTLog9AN9m9USFpWRCMlU8iMCVwACbQQAAjYSmFa-LfaOxMHalzME")
+        await message.reply_sticker("CAACAgIAAxkBAAEXfnxoXs0yNl9utJxH2a4JGVnGPkqRjQACbgADrWW8FJnSlTmh6LmHNgQ")
         await message.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
