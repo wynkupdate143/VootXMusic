57fe647e94a510c056a476ccebe18d970421a85e
AkashXMusic <amansingh093789@gmail.com>
2025-07-04 20:41:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,15 +35,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/nonitacode/ShizukaMusicBot4",
+    "https://github.com/wynkupdate143/VootXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AboutBotMaker")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/KING_BOT_UPDATE")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BOTXBAZAAR")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BOTXBAZAAR")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
